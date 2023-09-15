80276d51b006ca5fa1b39af7e67b8dd70cd4febe
parisollie <pjff18@gmail.com>
2023-09-15 00:11:01 -0600

    Initial commit

---
+++ app/src/main/java/com/pjff/videogamesrf/application/VideoGamesRFApp.kt
@@ -0,0 +1,16 @@
+package com.pjff.videogamesrf.application
+
+import android.app.Application
+
+
+class VideoGamesRFApp: Application() {
+
+    /*private val retrofit by lazy{
+        RetrofitHelper().getRetrofit()
+    }
+
+    val repository by lazy{
+        GameRepository(retrofit)
+    }*/
+
+}
